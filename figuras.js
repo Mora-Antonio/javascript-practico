666c62fe24b0044dc6afc0f1bf9f36e46d539b92
Mora-Antonio <miguel.antonio290120@gmail.com>
2022-06-03 23:48:57 -0600

    terminamos

--- figuras.js
+++ figuras.js
@@ -44,3 +44,81 @@ function triangleAnswers(){
     let area = areaTriangulo(base, height);
     areaAnswer.innerHTML = area;
 }
+
+//Funciones cambiar vista
+let circle = document.getElementById('circleSection');
+let triangle = document.getElementById('triangleSection');
+let square = document.getElementById('squareSection');
+
+function circleWindows(){
+    triangle.style.display = 'none';
+    square.style.display = 'none';
+    circle.style.display = 'flex';
+}
+function triangleWindows(){
+    triangle.style.display = 'flex';
+    square.style.display = 'none';
+    circle.style.display = 'none';
+}
+
+function squareWindows(){
+    triangle.style.display = 'none';
+    square.style.display = 'flex';
+    circle.style.display = 'none';
+}
+
+//Respuesta Circulo
+function circleAnswers(){
+    let radio = document.getElementById('radio');
+    let perimetroAnswer = document.getElementById('answerPerimeterCircle');
+    let areaAnswer = document.getElementById('answerAreaCircle');
+    // Valores de los input
+    let valueRadio = parseFloat(radio.value);
+    //Respuestas
+    let perimetro = perimetroCirculo(valueRadio);
+    perimetroAnswer.innerHTML = perimetro;
+    let area = areaCirculo(valueRadio);
+    areaAnswer.innerHTML = area;
+}
+//Respuesta Cuadrado
+function squareAnswers(){
+    let side = document.getElementById('sides');
+    let perimetroAnswer = document.getElementById('answerAreaSquare');
+    let areaAnswer = document.getElementById('answerPerimeterSquare');
+    // Valores de los input
+    let valueSides = parseFloat(side.value);
+    //Respuestas
+    let perimetro = perimetroCuadrado(valueSides);
+    perimetroAnswer.innerHTML = perimetro;
+    let area = areaCuadrado(valueSides);
+    areaAnswer.innerHTML = area;
+}
+
+function delte(){
+    let triangleHeight = document.getElementById('height');
+    let triangleBase = document.getElementById('base');
+    let triangleSideA = document.getElementById('sideA');
+    let triangleSideB = document.getElementById('sideB');
+    let radio = document.getElementById('radio');
+    let side = document.getElementById('sides');
+
+    let perimetroAnswerCircle = document.getElementById('answerPerimeterCircle');
+    let areaAnswerCircle = document.getElementById('answerAreaCircle');
+    let perimetroAnswerSquare = document.getElementById('answerAreaSquare');
+    let areaAnswerSquare = document.getElementById('answerPerimeterSquare');
+    let perimetroAnswer = document.getElementById('answerPerimeter');
+    let areaAnswer = document.getElementById('answerArea');
+
+    triangleHeight.value = '';
+    triangleBase.value = '';
+    triangleSideA.value = '';
+    triangleSideB.value = '';
+    radio.value = '';
+    side.value = '';
+    perimetroAnswerCircle.innerHTML = '';
+    areaAnswerCircle.innerHTML = '';
+    perimetroAnswerSquare.innerHTML = '';
+    areaAnswerSquare.innerHTML = '';
+    perimetroAnswer.innerHTML = '';
+    areaAnswer.innerHTML = '';
+}
